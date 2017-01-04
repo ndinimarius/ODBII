350818804ca20131c74845b34fa48f121b04e100
marius <marius.ndini@sap.com>
2017-01-04 09:53:01 -0500

    Initial Commit

---
+++ connectedCarTests/connectedCarTests.swift
@@ -0,0 +1,36 @@
+//
+//  connectedCarTests.swift
+//  connectedCarTests
+//
+//  Created by i830729 on 1/4/17.
+//  Copyright © 2017 i830729. All rights reserved.
+//
+
+import XCTest
+@testable import connectedCar
+
+class connectedCarTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
